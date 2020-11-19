c7c30693c4d23f60c4cb353d4fa7c041fc576d98
GTechnology-biz <gtech@gtechnology.biz>
2020-11-19 03:11:55 -0700

    newish app working :) yay

--- app.py
+++ app.py
@@ -6,7 +6,9 @@ import json
 import requests
 import datetime
 
-date = datetime.datetime.now().strftime("%m/%d/%Y")
+#date = datetime.datetime.now().strftime("%m/%d/%Y")
+date = "11/16/2020"
+
 fet = "features"
 att = "attributes"
 
@@ -14,21 +16,20 @@ state_daily_cnty_inf = "https://services3.arcgis.com/66aUo8zsujfVXRIT/arcgis/res
 state_daily_url = "https://services3.arcgis.com/66aUo8zsujfVXRIT/arcgis/rest/services/colorado_covid19_daily_state_statistics_cumulative/FeatureServer/0/query?where=Date%20%3D%20'" + date + "'&outFields=*&outSR=4326&f=json"
 state_exp_url = "https://services3.arcgis.com/66aUo8zsujfVXRIT/arcgis/rest/services/CDPHE_COVID19_StateLevel_Expanded_Case_Data_Summary/FeatureServer/0/query?where=date%20%3D%20%27" + date + "%27&outFields=section,category,description,date,metric,value&outSR=4326&f=json"
 
-
 app = dash.Dash(__name__)
 
-url = (state_daily_cnty_inf, state_daily_url, state_exp_url)
+urls = [state_daily_cnty_inf, state_daily_url, state_exp_url]
 nope = ""
 
-def url2json(url):
 
+def url2json(url):
+    print(url)
     r = requests.get(url)
     data = r.json()
     url_json = data
     return url_json
 
 def data_prep(url):
-
     nope = url2json(url)
     df = pd.json_normalize(nope, 'features')
     df =df.drop(columns=[
@@ -60,30 +61,41 @@ def data_prep(url):
 
 
 
-for i in url:
-    df = data_prep(i)
+for i in urls:
 
-    app.layout = dash_table.DataTable(
-         id='table',
-         columns=[
-             {"name": str(i), "id": str(i)} for i in df.columns
-             ],
-         data=df.to_dict('records') ,
-         style_cell={'textAlign': 'left'},
-         style_data_conditional=[
-             {
-                 'if': {'row_index': 'odd'},
-                 'backgroundColor': 'rgb(248, 248, 248)'}
-             ],
-         fixed_columns={'headers': True, 'data': 1},
-         style_table={'minWidth': '100%'},
-    
-         style_data={
+    nope = url2json(i)
+    df = pd.json_normalize(nope, 'features')
+    df =df.drop(columns=[
+        'attributes.GEOID',
+        'attributes.Shape__Length',
+        'attributes.Shape__Area',
+        'geometry.rings',
+        'attributes.Data_Source'
+        ], errors = 'ignore')
+    df = df.T.rename_axis('Stat').reset_index()
+
+    i = html.Div([
+        dash_table.DataTable(
+            id='urls',
+            columns=[
+                {"name": str(i), "id": str(i)} for i in df.columns
+                ],
+            data=df.to_dict('records') ,
+            style_cell={'textAlign': 'left'},
+            style_data_conditional=[
+                {
+                    'if': {'row_index': 'odd'},
+                    'backgroundColor': 'rgb(248, 248, 248)'}
+                ],
+            fixed_columns={'headers': True, 'data': 1},
+            style_table={'maxWidth': '80%'},
+            style_data={
              'whiteSpace': 'normal',
              'height': 'auto'},
-    
-    
-         )
+
+            )])
+app.layout = html.Div(children=[urls])
+
 
 
 if __name__ == '__main__':
